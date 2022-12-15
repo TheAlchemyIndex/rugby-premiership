956e8727681042cfac297e9871782e1daebf4be8
TheAlchemyIndex <adam.vaughan@hotmail.co.uk>
2022-12-15 21:29:04 +0000

    changes

--- predictor/ResultPredictor.py
+++ predictor/ResultPredictor.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from predictor.calculators.CalcMeanColumns import calculate_means, calculate_conditional_means_two_cols, \
+from calculators.CalcMeanColumns import calculate_means, calculate_conditional_means_two_cols, \
     calculate_conditional_means_three_cols
 from predictor.filters.ScoreFilter import filter_numeric_scores
 
 
-FILE_PATH = r"/data/All Seasons - 2010-23.csv"
+FILE_PATH = r"../data/All Seasons - 2010-23.csv"
 
 df = pd.read_csv(FILE_PATH)
 df_filtered = filter_numeric_scores(df)
