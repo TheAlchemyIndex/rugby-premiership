7b6adbc347166fe8ef4ef2331f29d0c3ab930e72
TheAlchemyIndex <adam.vaughan@hotmail.co.uk>
2023-01-15 22:17:55 +0000

    adding game type - home or away

--- srs/premiership/PremiershipModule.py
+++ srs/premiership/PremiershipModule.py
@@ -3,7 +3,7 @@ from srs.premiership.wikipedia.scraper import ScrapingModule
 
 
 def main():
-    #ScrapingModule.write("both", 2014, 15, 23)
+    ScrapingModule.write("recent", 2014, 15, 23)
     CalculationModule.create_calculated_columns(2014, 15, 23)
 
 
